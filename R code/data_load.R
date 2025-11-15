11372410b159b6412a79510592ba2d6f7a83cdac
Andrii Tsokol <atsokol@gmail.com>
2025-11-15 21:10:47 +0100

    comm

--- R code/data_load.R
+++ R code/data_load.R
@@ -26,8 +26,7 @@ price_dam <- read.csv(text = rawToChar(response$content))
 
 write_csv(price_dam, "data/outputs/DAM_prices.csv")
 
-dates <- range(price_dam$date) |> 
-  format("%Y%m%d")
+dates <- c(ymd("2018/01/01"), Sys.Date()) |> format("%Y%m%d")
 
 # Get data and convert to xts object
 fx <- get_exchange_rate_date_range_currency(dates[1],dates[2], 'EUR') %>% 
